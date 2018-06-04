5cd9757ce157828d672c159b1732986570008d79
gustavo.oavergani <gustavo.oavergani@TAT0541226W10-1.senacsp.edu.br>
2018-06-04 17:01:12 -0300

    Calcular Area do Circulo

---
+++ Fundamentos do Java/src/br/com/java/Circulo.java
@@ -0,0 +1,43 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class Circulo {
+
+	public static void main(String[] args) {
+		// Calcular Ciruclo
+		double area, raio, pi;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Calcular Area do Circulo: ");
+		System.out.println();
+		//Entrada
+		System.out.println("Insira o Raio: ");
+		raio = teclado.nextDouble();
+		pi = (3.14);
+		//Processos
+		area = pi * (raio * raio);
+		//Saida
+		System.out.println("Area do Circulo" + area);
+		
+		
+		
+				
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+
+}
