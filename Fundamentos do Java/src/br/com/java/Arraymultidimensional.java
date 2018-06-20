b4291124202c8e78cc6a97b78731b0c4ca92195b
gustavo.oavergani <gustavo.oavergani@TAT0541226W10-1.senacsp.edu.br>
2018-06-20 14:27:41 -0300

    Array multidimensional

---
+++ Fundamentos do Java/src/br/com/java/Arraymultidimensional.java
@@ -0,0 +1,17 @@
+package br.com.java;
+
+public class Arraymultidimensional {
+
+	public static void main(String[] args) {
+		// a linha abaixo cria um array multidimensional(matriz)
+		double[][] boletim = {{9,7,9,3},{4,5,8,6}};
+		// recuperando a nota de portugues do 3º Bimestre
+		System.out.println("Médio de Portugues");
+		
+		
+		
+		
+
+	}
+
+}
